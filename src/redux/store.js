11c2a2f831353cf24f5bcc43d59aa93eda526d3e
DangThiemLoc <thiemloc02@gmail.com>
2024-01-25 23:53:44 +0700

    done CreateUser

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UsersData from "./userlistManagement/userSlice";
 import ProfileSlice from "./profile/ProfileSlice";
-import UserData from "./userlistManagement/CreateUserSlice";
+
 export default configureStore({
     reducer: {
         userlist: UsersData,
         viewProfile: ProfileSlice,
-        user: UserData,
     }
 })
