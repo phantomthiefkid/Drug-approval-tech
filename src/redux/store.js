7967de48aa6cd96084d6931dfd4f111fdff0028d
DangThiemLoc <thiemloc02@gmail.com>
2024-02-02 21:12:03 +0700

    list drug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UsersData from "./userlistManagement/userSlice";
 import ProfileSlice from "./profile/ProfileSlice";
-import DrugsData from "./drugManagement/drugSlice";
+import DrugsData from "./drugManagement/drugSlice"
 
 export default configureStore({
     reducer: {
         userlist: UsersData,
         viewProfile: ProfileSlice,
-
+        druglist: DrugsData
     }
 })
