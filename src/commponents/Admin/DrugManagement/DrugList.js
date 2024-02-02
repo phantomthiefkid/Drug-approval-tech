811679b1b065345405b9190bf6259c0bb8b37f68
DangThiemLoc <thiemloc02@gmail.com>
2024-02-02 22:13:00 +0700

    done drugList

--- src/commponents/Admin/DrugManagement/DrugList.js
+++ src/commponents/Admin/DrugManagement/DrugList.js
@@ -8,7 +8,7 @@ import { fetchDrugs } from '../../../redux/drugManagement/drugSlice'
 const DrugList = () => {
 
     const drugsAPI = useSelector((drug) => drug.druglist)
-    const totalPages = useSelector((state) => state.druglist);
+    const [totalPages, setTotalPages] = useState(0);
     const dispatch = useDispatch();
     const [isOpenFilter, setIsOpenFilter] = useState(false);
     const [currentPage, setCurrentPage] = useState(0);
@@ -16,8 +16,9 @@ const DrugList = () => {
     const [drugList, setDrugList] = useState([]);
     const [sortField, setSortField] = useState('');
     const [sortOrder, setSortOrder] = useState('asc');
+    const [searchTerm, setSearchTerm] = useState('');
 
-    let count = 1
+    const itemsPerPage = 8
 
     useEffect(() => {
         if (Array.isArray(drugsAPI)) {
@@ -31,20 +32,22 @@ const DrugList = () => {
             pageNo: currentPage,
             sortField,
             sortOrder,
+            search: searchTerm
 
         })).then((response) => {
             if (response.payload.content.length === 0) {
-                // Nếu không có dữ liệu, set lại currentPage về 0
                 setCurrentPage(0);
+                setTotalPages(0);
             } else {
                 setApiData(response.payload.content);
+                setTotalPages(response.payload.totalPages);
             }
         })
             .catch((error) => {
                 // Xử lý lỗi nếu có
                 console.error('Error fetching users:', error);
             });
-    }, [dispatch, currentPage, sortField, sortOrder]);
+    }, [dispatch, currentPage, sortField, sortOrder, searchTerm]);
     useEffect(() => {
         setDrugList([...apiData]);
     }, [apiData]);
@@ -60,6 +63,10 @@ const DrugList = () => {
     const handlePageChange = (page) => {
         setCurrentPage(page);
     };
+
+    const toggleFilter = () => {
+        setIsOpenFilter(!isOpenFilter)
+    }
     const resetFilters = () => {
         setSortField('')
         setIsOpenFilter(false);
@@ -70,6 +77,7 @@ const DrugList = () => {
 
     const handleSortFieldChange = (e) => setSortField(e.target.value);
     const handleSortOrderChange = (e) => setSortOrder(e.target.value);
+    const handleSearchChange = (e) => setSearchTerm(e.target.value);
 
     return (
         <>
@@ -92,39 +100,38 @@ const DrugList = () => {
                                     </svg>
                                 </div>
                                 <input
-                                    type="search" name='search' id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg " placeholder="Tìm kiếm..." required />
+                                    type="search" name='search' id="default-search" value={searchTerm} onChange={handleSearchChange} class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg " placeholder="Tìm kiếm..." required />
                             </div>
                         </form>
 
-                        <div className='w-1/4 ml-2 flex'><button className='text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'><Filter size={30}></Filter> </button>
+                        <div className='w-1/4 ml-2 flex'><button onClick={toggleFilter} className='text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'><Filter size={30}></Filter> </button>
                             {isOpenFilter && (
                                 <div className="origin-top-right absolute z-10 ml-14 w-72 rounded-md shadow-xl bg-white ring-1 ring-black ring-opacity-5">
                                     <div className="py-4 px-6" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                         <h3 className="text-lg font-semibold mb-2">Filter Form</h3>
-                                        {/* Sort Field */}
                                         <div className='flex w-full'>
                                             <div className="mb-4 w-full">
                                                 <label htmlFor="sortField" className="block text-sm font-medium text-gray-700">Sort Field</label>
-                                                <select id="sortField" className="mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:ring focus:border-blue-300">
-                                                    <option value="">Choose</option>
-                                                    <option value="fullname">Full Name</option>
-                                                    <option value="email">Email</option>
-                                                    <option value="username">Username</option>
+                                                <select value={sortField} onChange={handleSortFieldChange} id="sortField" className="mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:ring focus:border-blue-300">
+                                                    <option value="">Chọn</option>
+                                                    <option value="name">Tên</option>
+                                                    <option value="type">Loại</option>
+                                                    <option value="state">Tình trạng</option>
                                                 </select>
                                             </div>
 
-                                            {/* Sort Order */}
                                             <div className="mb-4 ml-4 w-full">
                                                 <label htmlFor="sortOrder" className="block text-sm font-medium text-gray-700">Sort Order</label>
-                                                <select id="sortOrder" className="mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:ring focus:border-blue-300">
-                                                    <option value="asc">Ascending</option>
-                                                    <option value="desc">Descending</option>
+                                                <select value={sortOrder} onChange={handleSortOrderChange} id="sortOrder" className="mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:ring focus:border-blue-300">
+                                                    <option value=''>Chọn</option>
+                                                    <option value="asc">Tăng dần</option>
+                                                    <option value="desc">Giảm dần</option>
                                                 </select>
                                             </div>
                                         </div>
 
                                         <div className="flex justify-end">
-                                            <button className='text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 me-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800'>
+                                            <button onClick={resetFilters} className='text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 me-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800'>
                                                 Reset
                                             </button>
                                             <button onClick={() => setIsOpenFilter(false)} className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-300 rounded-md hover:bg-gray-400 focus:outline-none focus:ring focus:border-gray-500">
@@ -139,13 +146,11 @@ const DrugList = () => {
                 </div>
 
                 <div className='mb-6'>
-
-
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                         <table class="w-5/6 shadow-2xl mb-12 table-auto mx-auto text-sm text-left rtl:text-right text-gray-500">
                             <thead class="text-xs text-white uppercase bg-blue-900">
                                 <tr>
-                                    <th scope="col" class="px-2 py-3">
+                                    <th scope="col" class="px-2 py-3 ">
                                         Số thứ tự
                                     </th>
                                     <th scope="col" class="px-2 py-3">
@@ -166,9 +171,9 @@ const DrugList = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                {Array.isArray(drugList) && drugList.map((drug) => (<tr class="bg-white border-b hover:bg-gray-100">
+                                {Array.isArray(drugList) && drugList.map((drug, index) => (<tr class="bg-white border-b hover:bg-gray-100">
                                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                        {count++}
+                                        {index + 1 + currentPage * itemsPerPage}
                                     </th>
                                     <td class="">
                                         {drug.name}
@@ -182,10 +187,7 @@ const DrugList = () => {
                                     <td class="">
                                         {drug.simpleDescription}
                                     </td>
-
                                 </tr>))}
-
-
                             </tbody>
                         </table>
                         <div className='mb-6 flex justify-center'>
@@ -217,7 +219,6 @@ const DrugList = () => {
                         </div>
                     </div>
                 </div>
-
             </div>
         </>
     )
